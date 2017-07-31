d7963e7af82cf82291e172a9deab35ebdb4cd47d
horngyih <horngyih@yahoo.com>
2017-07-31 19:53:13 +0800

    Commiting a half-completed swagger definition, and added a mock handler for /member route

---
+++ api/controllers/ubiq_cms_member.js
@@ -0,0 +1,25 @@
+'use string';
+
+function memberDetails(req, res){
+    var customerID = req.swagger.params.customerID.value || '';
+    var posTerminalID = req.swagger.params.posTerminalID.value || '';
+
+    if( customerID !== 'NOBODY' ){
+        var member = {
+            customerID : customerID,
+            posTerminalID : posTerminalID,
+            membershipNo : '123',
+            memberName : 'Doe, John',
+            memberStatus : 'ACTIVE',
+            redemptionBalance : 1989.00
+        };
+        res.json( member );
+    } else {
+        res.status(404);
+        res.json( "Member Not Found" );
+    }
+}
+
+module.exports = {
+    memberDetails: memberDetails
+};
